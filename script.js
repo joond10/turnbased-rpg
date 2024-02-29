be2ed2d1017b0f65e002f0a99f85ce060dfdc6cd
Joon <thdong1@myseneca.ca>
2024-02-29 12:22:50 -0500

    Starting defend function

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ let currentHealth = initialHealth;
 let fullHealthBarWidth = 300; // (px)
 let currentHealthBarWidth = fullHealthBarWidth;
 let defenceStance = false;
+let attack = document.querySelector("#attack");
+let begin = document.querySelector("#begin");
+let defend = document.querySelector("#defend");
+let animation = document.querySelectorAll("img");
 
 function playerAttackAnimation() {
-  let animation = document.querySelectorAll("img");
   animation[0].src = "img/playerattack.png";
+}
+
+function fightingStanceAnimation() {
+  animation[0].src = "img/playerfightingstance.png";
+  animation[1].src = "img/enemyfightingstance.png";
 }
 
 function enemyReceiveDamage() {
@@ -18,19 +26,26 @@ function enemyReceiveDamage() {
   health.innerText = currentHealth + "HP";
   if (currentHealth < 70) {
     health.style.backgroundColor = "darkorange";
+    //tiredanimation()
     if (currentHealth < 50) {
       health.style.backgroundColor = "darkred";
+      //weakanimation()
     }
   }
 }
 
-let attack = document.querySelector("#attack");
 attack.addEventListener("click", function () {
   playerAttackAnimation();
-  let speech = document.querySelector("#speech");
-  speech.innerText = "AGH";
+  let enemyspeech = document.querySelector("#enemy-speech");
+  enemyspeech.innerText = "AGH";
   enemyReceiveDamage();
 });
 
-let begin = document.querySelector("#begin");
-begin.addEventListener("click", function () {});
+begin.addEventListener("click", function () {
+  let playerspeech = document.querySelector("#player-speech");
+  playerspeech.setAttribute("hidden", true);
+  attack.removeAttribute("hidden");
+  defend.removeAttribute("hidden");
+  begin.setAttribute("hidden", true);
+  fightingStanceAnimation();
+});
